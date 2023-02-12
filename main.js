3f1b9236624d93997923bcd7b9c63dad72651a1e
allyhere <camilomichelettors@gmail.com>
2023-02-12 02:03:17 -0300

    fix: changed video settings

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ const video = document.querySelector("[data-video]");
 navigator.mediaDevices
   .getUserMedia({
     audio: false,
-    video: { width: 400, height: 600, facingMode: "environment" },
+    video: {
+      width: { exact: 300 },
+      height: { exact: 500 },
+      facingMode: "user",
+    },
   })
   .then((stream) => {
     video.srcObject = stream;
     video.onloadedmetadata = () => video.play();
   })
   .catch((err) => {
-    console.log({ err });
+    alert("You have to enable camera permissions!");
   });
